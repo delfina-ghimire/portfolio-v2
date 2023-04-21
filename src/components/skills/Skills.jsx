88475cae67d56f333a02a88cf616a26281a9976f
delfina <delfina.ghimire@gmail.com>
2023-04-21 07:39:44 +0545

    feat: style skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,22 +1,25 @@
-import SectionTitle from "./commons/SectionTitle";
-import { skills } from "../constants";
+import SectionTitle from "../commons/SectionTitle";
+import { skills } from "./constants";
 
 const Skills = () => {
   return (
     <div>
       <SectionTitle title="Skills" />
-      <section className="flex pt-28">
+      <section className="flex pt-[10rem]">
         {skills.map((skill) => (
-          <div className="text-center relative  ">
-            <div className="w-24 h-24  absolute left-[30%] ">
+          <div
+            key={skill.id}
+            className="flex-1 flex flex-col items-center justify-start  "
+          >
+            <div className="w-[100px] h-[100px] ">
               <img
                 className="w-full h-full"
                 src={skill.icon}
-                alt="development"
+                alt={skill.title}
               />
             </div>
-            <div className="mt-24">
-              <h4 className="text-3xl px-8 py-6">{skill.title}</h4>
+            <div className="text-center">
+              <h4 className="text-[2rem] font-bold px-8 py-6">{skill.title}</h4>
               <ul className="text-gray-600 text-center text-3xl leading-10">
                 {skill.list.map((item) => (
                   <div>{item}</div>
