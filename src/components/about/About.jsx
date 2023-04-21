fdce6e0849d677360e80bea9af324de000fe42cf
delfina <delfina.ghimire@gmail.com>
2023-04-21 06:52:56 +0545

    feat: style about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import SectionTitle from "./commons/SectionTitle";
-import aboutImg from "../assets/delfina.png";
+import SectionTitle from "../commons/SectionTitle";
+import aboutImg from "../../assets/delfina.png";
+import styles from "../../style";
 
 const About = () => {
   return (
     <>
-      <SectionTitle title="About Me" />
-      <section>
+      <div className="pb-[10rem]">
+        <SectionTitle title="About Me" />
+      </div>
+
+      <section className={styles.sectionPadding}>
         <div className="flex items-center justify-center">
-          <div className="w-64 h-auto">
+          <div className="w-[90rem] h-auto">
             <img className="w-full h-full" src={aboutImg} alt="" />
           </div>
-          <div className="text-center py-5 px-5">
+          <div className="text-center py-4 px-[4rem] text-[2rem]">
             <p className="text-slate-500">
               <b>
                 I'm a self taught web developer passionate about crafting
@@ -29,7 +32,7 @@ const About = () => {
               <br />
               <br />
               <b> When I'm not in front of a computer screen,</b> I'm probably
-              reading new book, traveling, practicing Spanish or volunteering.
+              reading a new book, traveling or practicing Spanish.
             </p>
           </div>
         </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Navbar from "./navbar/Navbar.jsx";
 import Hero from "./hero/Hero.jsx";
-import About from "./About";
-import Skills from "./Skills";
+import About from "./about/About.jsx";
+import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import ContactMe from "./contactMe/ContactMe";
 
