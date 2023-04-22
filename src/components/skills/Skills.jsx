46c8b75df693dd990573dadf97222e7de8bc2923
delfina <delfina.ghimire@gmail.com>
2023-04-22 23:02:22 +0545

    responsove:skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <div>
       <SectionTitle title="Skills" />
-      <section className="flex pt-[10rem]">
+      <section className="flex flex-col gap-16 md:gap-0 md:flex-row pt-10 md:pt-[10rem]">
         {skills.map((skill) => (
           <div
             key={skill.id}
@@ -19,8 +19,10 @@ const Skills = () => {
               />
             </div>
             <div className="text-center">
-              <h4 className="text-[2rem] font-bold px-8 py-6">{skill.title}</h4>
-              <ul className="text-gray-600 text-center text-3xl leading-10">
+              <h4 className="text-[1.5rem] md:text-[2rem] font-bold px-8 -py-[-2rem] md:py-6">
+                {skill.title}
+              </h4>
+              <ul className="text-gray-600 text-center text-2xl md:text-3xl leading-10">
                 {skill.list.map((item) => (
                   <div>{item}</div>
                 ))}
