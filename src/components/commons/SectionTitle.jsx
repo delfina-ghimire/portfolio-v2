1ac6d6caf13b9b65a0f783ae4fae839960964b1f
delfina <delfina.ghimire@gmail.com>
2023-04-22 22:19:11 +0545

    responsive: section title

--- src/components/commons/SectionTitle.jsx
+++ src/components/commons/SectionTitle.jsx
@@ -1,10 +1,10 @@
 const SectionTitle = (props) => {
   return (
     <section className="flex items-center justify-center relative">
-      <h2 className="text-gray-900 text-6xl font-bold leading-10">
+      <h2 className="text-gray-900 md:text-6xl text-5xl font-bold leading-10">
         {props.title}
       </h2>
-      <span className="w-16 h-1 absolute bg-indigo-700 bottom-[-25px] left-[48%]"></span>
+      <span className="w-16 md:h-1  h-[2px] absolute bg-indigo-700 md:bottom-[-20px] bottom-[-8px] left-[48%]"></span>
     </section>
   );
 };
