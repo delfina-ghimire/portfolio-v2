91ca75947fb8f219289ec5b2949ecdc4d40dad11
delfina-ghimire <delfina.ghimire@gmail.com>
2023-05-01 13:45:32 +0545

    feat: add blog section

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -1,5 +1,6 @@
 import SectionTitle from "../commons/SectionTitle";
-import { BsArrowRight } from "react-icons/bs";
+import { BsArrowRight, BsArrowRightShort } from "react-icons/bs";
+import { blogs } from "./constants";
 
 const Blog = () => {
   return (
@@ -8,58 +9,48 @@ const Blog = () => {
         <div id="Blogs" className="py-[10rem] md:pb-[4rem]">
           <SectionTitle title="Blogs" />
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-          <div class="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
-            <div class="relative flex items-end overflow-hidden rounded-xl">
-              <img
-                src="https://thumbnails.production.thenounproject.com/gA9eZOvsBYSHrMumgrslmRGoBto=/fit-in/1000x1000/photos.production.thenounproject.com/photos/BCBA88B6-5B41-4B50-A786-E60CAA0ECDA3.jpg"
-                alt="wallpaper"
-              />
-            </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24 p-6">
+          {blogs.map((blog) => (
+            <div class="flex flex-col cursor-pointer rounded-xl bg-white shadow-lg hover:shadow-xl py-5 px-5">
+             <div className="flex-1">
+             <div class="relative flex items-end overflow-hidden rounded-xl">
+                <img key={blog?.id} src={blog?.image} alt="wallpaper" />
+              </div>
 
-            <div class="mt-1 p-2">
-              <h2 class="text-slate-700">The Malta Hotel</h2>
-              <p class="mt-1 text-sm text-slate-400">Italy, Europe</p>
+              <div class="mt-5">
+                <h2 class="text-slate-700 text-2xl">{blog.title}</h2>
+                <p class="mt-1 text-lg text-slate-400">{blog.category}</p>
+              </div>
+              <div className="my-[1rem]">
+                <p class="text-gray-500 text-[1.5rem]">{blog.desc}</p>
+              </div>
+              </div>
+             
+              <div class="flex items-center flex-wrap ">
+                <button class={`text-white bg-gradient-to-r ${blog.colors.from} ${blog.colors.to} hover:scale-105 drop-shadow-md shadow-cla-blue px-4 py-2 rounded-lg text-xl flex flex-row`}>
+                  <span>Read more</span>
+                  <span><BsArrowRightShort size={20}/>
+                  </span>
+
+                </button>
+              </div>
             </div>
-            <div className="my-[1rem]">
-              <p class="text-gray-500 text-md">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Incidunt sapiente totam sequi repellendus molestiae. Accusamus
-                quasi, temporibus libero nostrum tempore tempora, blanditiis
-                minima cum praesentium deleniti facilis ex alias magnam.
-              </p>
-            </div>
-            <div class="flex items-center flex-wrap ">
-              <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">
-                Read more
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-
-      <div class="flex items-center flex-wrap ">
-        <button class="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg">
-          Learn more
-        </button>
-      </div>
-      <div className="flex justify-center text-center border border-red-900">
+      <div className="flex justify-center items-center">
         <a
           href="#"
-          className="flex flex-row w-96 mt-6 text-purple-500 hover:text-purple-400"
+          className="flex flex-row my-6 text-blue-500 hover:text-blue-400"
         >
-          <span>Read More</span>
+          <span>Explore More</span>
           <span className="ml-2 mt-2">
             <BsArrowRight />
           </span>
         </a>
       </div>
       <div class="mt-6 mb-6 h-px bg-slate-200"></div>
-      <div class="flex items-center justify-center flex-wrap ">
-        <button class="bg-gradient-to-r from-fuchsia-300 to-pink-400 hover:scale-105  shadow-cla-blue px-4 py-1 rounded-lg">
-          Learn more
-        </button>
-      </div>
+    
     </>
   );
 };
--- src/components/blogs/constants.js
+++ src/components/blogs/constants.js
@@ -1,17 +1,20 @@
-import project1 from "../../assets/p1.png";
-import project2 from "../../assets/p2.png";
-import project3 from "../../assets/p3.png";
-import project4 from "../../assets/p4.png";
+import blog1 from "../../assets/b1.png";
+import blog2 from "../../assets/b2.png";
+import blog3 from "../../assets/b3.png";
 import github from "../../assets/icons/github.svg";
 import live from "../../assets/icons/external-link.svg";
 
-export const projects = [
+export const blogs = [
   {
-    image: project1,
-    id: "project-1",
-    title: "FriendsBook",
-    desc: "A Fullstack social media application with CRUD functionality for posts & users, view friends post on your timeline, follow/unfollow users, like/dislike posts, get news and follow recomendations.",
-    techs: ["React", "MongoDB", "Express", "Node js"],
+    image: blog1,
+    id: "blog-1",
+    title: "What happens to CSS when we load up a webpage?",
+    desc: "Do you find CSS simple and hard at the same time? If so, you’re not alone. Understanding the inner workings of CSS is key to unlocking its full potential. In this post, I’ll share the theoretical concepts of ‘How CSS actually works behind the scenes’ that have helped me to grasp CSS better.",
+    category: "CSS3",
+    colors: {
+      from: 'from-orange-300',
+      to: 'to-amber-400',
+    },
     icons: [
       {
         src: github,
@@ -26,11 +29,11 @@ export const projects = [
     ],
   },
   {
-    image: project2,
-    id: "project-2",
-    title: "SandBox - NFT Website",
-    desc: "SandBox is the Modern UX / UI Site that allows you to the explore, create, sale, and purchase the NFTs. It consists the tuned blend of multiple complex gradiends and graphics.",
-    techs: ["React", "Tailwind css"],
+    image: blog2,
+    id: "blog-2",
+    title: "Animation in React: React-Spring",
+    desc: "If you’re looking to create animations in React, React Spring is one of the most popular animation libraries to create powerful animation easily.It can be used to animate HTML, SVG, Native Elements, Three.js, and more.",
+    category: "React Animation",
     icons: [
       {
         src: github,
@@ -43,13 +46,17 @@ export const projects = [
         link: "https://sandbox-nft.netlify.app/",
       },
     ],
+    colors: {
+      from: 'from-cyan-500',
+      to: 'to-blue-600',
+    },
   },
   {
-    image: project3,
-    id: "project-3",
-    title: "Avacodo: Ecommerce Website",
-    desc: "It is a Responsive Multipage Ecommerce site with an attractive modern theme, and complex layered footer. The code is arranged in scalable organized file architecture and is written using BEM naming convention.",
-    techs: ["React", "MongoDB", "Express", "Node js"],
+    image: blog3,
+    id: "blog-3",
+    title: "Konva for Beginners: An Introduction",
+    desc: "Konva is a 2D JavaScript drawing library built on the HTML5 canvas element for creating complex shapes, animations, and interactions on the web. It provides high performance, ease of use, and cross-platform compatibility, with React Konva offering a higher-level abstraction for use within React applications.",
+    category: "Canvas / React",
     icons: [
       {
         src: github,
@@ -62,24 +69,9 @@ export const projects = [
         link: "https://avocado-full-ecommerce-store.vercel.app/",
       },
     ],
-  },
-  {
-    image: project4,
-    id: "project-4",
-    title: "Natours - Travel Website",
-    desc: "A modern Travel Agency webpage built only using Html and css, written using BEM convention and followed 7-1 architecture pattern. Fluid Layouts, Responsive & flexible images,,Correct Units are also implemented.",
-    techs: ["Html", "CSS"],
-    icons: [
-      {
-        src: github,
-        alt: "View Code",
-        link: "https://github.com/delfina-ghimire/Natours-project",
-      },
-      {
-        src: live,
-        alt: "View Live",
-        link: "https://natours-travel-website.netlify.app/",
-      },
-    ],
+    colors: {
+      from: 'from-fuchsia-400',
+      to: 'to-pink-500',
+    },
   },
 ];
